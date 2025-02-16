f44d648753965b36b798c2b2fe74804a5cfc6320
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-02-16 22:13:24 +0100

    ęęęę

--- modules/desktop/wayland.nix
+++ modules/desktop/wayland.nix
@@ -1,8 +1,9 @@
 _: {
   environment.variables = {
+    CLUTTER_BACKEND = "wayland";
     GDK_BACKEND = "wayland,x11,*";
+    NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORM = "wayland;xcb";
     SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
   };
 }
