b62d651acda7ad17b7072a9b8dc460a2f7e503a0
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-02-17 22:22:46 +0100

    ß

--- system/common/shell/zsh/config.zsh
+++ system/common/shell/zsh/config.zsh
@@ -1,1 +1,57 @@
-echo "hello from zsh"
+export HISTFILESIZE=16384
+export HISTSIZE=16384
+setopt HIST_FCNTL_LOCK
+unsetopt APPEND_HISTORY
+setopt HIST_IGNORE_DUPS
+unsetopt HIST_IGNORE_ALL_DUPS
+unsetopt HIST_SAVE_NO_DUPS
+unsetopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_SPACE
+unsetopt HIST_EXPIRE_DUPS_FIRST
+setopt SHARE_HISTORY
+setopt EXTENDED_HISTORY
+HISTFILE=~/.cache/zsh-history
+
+zvm_after_init_commands+=('eval "$(fzf --zsh)"')
+
+bindkey -v
+export KEYTIMEOUT=1
+
+export AUTO_NOTIFY_THRESHOLD=30
+export AUTO_NOTIFY_TITLE='`%command` has just finished'
+export AUTO_NOTIFY_BODY='It completed in %elapsed seconds with exit code %exit_code'
+
+autoload -U compinit
+zstyle ':completion:*' menu no
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':completion:*:git-checkout:*' sort false
+zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+zstyle ':fzf-tab:*' popup-pad 24 0
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color always $realpath'
+zmodload zsh/complist
+zsh-defer compinit
+
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q'
+preexec() { echo -ne '\e[5 q'; }
+
+function nr() { nix run "nixpkgs#$1" -- $(shift; echo "$@"); }
+function ns() { nix shell $(printf "nixpkgs#%s " "$@"); }
--- system/common/shell/zsh/default.nix
+++ system/common/shell/zsh/default.nix
@@ -1,18 +1,41 @@
-{pkgs, ...}: let
-  inherit (pkgs) makeWrapper symlinkJoin zsh writeShellScriptBin;
+{
+  lib,
+  pkgs,
+  theme,
+  ...
+}: let
+  inherit (builtins) toFile;
+  inherit (lib) concatStringsSep mapAttrsToList pipe;
+  inherit (pkgs) makeWrapper symlinkJoin writeShellScriptBin;
+  inherit (theme.colors) bg3;
   aliases' = "";
-  config = writeShellScriptBin ".zshrc" ''
-    eval "$(starship init zsh)"
-    source ${./config.zsh}
+  highlights' = toFile "highlights" (pipe (import ./highlights.nix {inherit theme;}) [
+    (mapAttrsToList (name: value: "ZSH_HIGHLIGHT_STYLES[${name}]=\'${value}\'"))
+    (concatStringsSep "\n")
+  ]);
+  config = writeShellScriptBin ".zshrc" (with pkgs;
+    # sh
+      ''
+        eval "$(starship init zsh)"
 
-    source ${pkgs.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh
+        source ${zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh
 
-    ${aliases'}
-  '';
+        source ${./config.zsh}
+        ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#${bg3}"
+        ${aliases'}
+
+        zsh-defer source ${zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+        source ${highlights'}
+        zsh-defer source ${zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+        zsh-defer source ${zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+        zsh-defer source ${zsh-autopair}/share/zsh/zsh-autopair/autopair.zsh
+        zsh-defer source ${zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+        zsh-defer source ${zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
+      '');
   zsh' =
     (symlinkJoin rec {
       name = "zsh";
-      paths = [zsh];
+      paths = with pkgs; [fzf lsd zsh];
       nativeBuildInputs = [makeWrapper];
       postBuild = ''
         wrapProgram $out/bin/${name} \
@@ -21,159 +44,3 @@
     })
     .overrideAttrs (_: {passthru.shellPath = "/bin/zsh";});
 in {user.shell = zsh';}
-/*
-{
-  config,
-  pkgs,
-  ...
-}: let
-  inherit (config.kkts.system) username;
-  inherit (config.stylix) base16Scheme;
-  orange = base16Scheme.base09;
-  gray = base16Scheme.base03;
-in {
-  programs.zsh.enable = true;
-  users.users.${username}.shell = pkgs.zsh;
-  home-manager.users.${username} = {
-    home.packages = with pkgs; [lsd];
-    programs.zsh = {
-      enable = true;
-      plugins = [
-        {
-          file = "share/fzf-tab/fzf-tab.zsh";
-          name = "zsh-fzf-tab";
-          src = pkgs.zsh-fzf-tab;
-        }
-      ];
-      autosuggestion = {
-        enable = true;
-        highlight = "fg=#${gray}";
-      };
-      defaultKeymap = "viins";
-      history = {
-        extended = true;
-        size = 16384;
-      };
-      sessionVariables = {
-        VISUAL = "$EDITOR";
-        PAGER = "less";
-      };
-      syntaxHighlighting = {
-        enable = true;
-        styles = let
-          error = "fg=red";
-          keyword = "fg=magenta";
-          command = "fg=blue";
-          substitution = "fg=cyan";
-          argument = "fg=yellow";
-          operator = "fg=magenta";
-          string = "fg=green";
-          variable = "fg=#${orange}";
-          path = "fg=cyan";
-          comment = "fg=#${gray}";
-        in {
-          unknown-token = error;
-          reserved-word = keyword;
-          alias = command;
-          suffix-alias = command;
-          global-alias = command;
-          builtin = keyword;
-          function = command;
-          inherit command;
-          precommand = keyword;
-          commandseparator = operator;
-          hashed-command = command;
-          autodirectory = path;
-          inherit path;
-          path_prefix = path;
-          globbing = operator;
-          history-expansion = keyword;
-          command-substitution = substitution;
-          command-substitution-unquoted = substitution;
-          command-substitution-quoted = substitution;
-          command-substitution-delimiter = substitution;
-          command-substitution-delimiter-unquoted = substitution;
-          command-substitution-delimiter-quoted = substitution;
-          process-substitution = substitution;
-          process-substitution-delimiter = substitution;
-          arithmetic-expansion = substitution;
-          single-hyphen-option = argument;
-          double-hyphen-option = argument;
-          back-quoted-argument = variable;
-          back-quoted-argument-unclosed = variable;
-          back-quoted-argument-delimiter = variable;
-          single-quoted-argument = string;
-          single-quoted-argument-unclosed = string;
-          double-quoted-argument = string;
-          double-quoted-argument-unclosed = string;
-          dollar-quoted-argument = string;
-          dollar-quoted-argument-unclosed = string;
-          rc-quote = operator;
-          dollar-double-quoted-argument = variable;
-          back-double-quoted-argument = operator;
-          back-dollar-quoted-argument = operator;
-          assign = variable;
-          redirection = operator;
-          inherit comment;
-          named-fd = operator;
-          numeric-df = operator;
-          arg0 = command;
-          default = argument;
-        };
-      };
-      shellAliases = with pkgs; {
-        sudo = "sudo ";
-        ls = "${lsd}/bin/lsd --group-directories-first";
-        ll = "${lsd}/bin/lsd -l --group-directories-first";
-        la = "${lsd}/bin/lsd -lA --group-directories-first";
-        lt = "${lsd}/bin/lsd --tree --group-directories-first";
-        lta = "${lsd}/bin/lsd -A -I .git --tree --group-directories-first";
-        cp = "${coreutils}/bin/cp -r";
-        scp = "${openssh}/bin/scp -r";
-        mkdir = "${coreutils}/bin/mkdir -p";
-        grep = "${ripgrep}/bin/rg";
-        cat = "${bat}/bin/bat --paging never --style numbers,changes --theme base16";
-        less = "${bat}/bin/bat --paging always --style numbers,changes --theme base16";
-        du = "${dust}/bin/dust -T $(nproc) -x";
-        df = "${duf}/bin/duf -hide special -hide-mp '/nix*,/persist'";
-        diff = "${diffutils}/bin/diff --color=auto";
-        ip = "${iproute2}/bin/ip --color=auto";
-        tmux-kill-inactive = ''
-          ${tmux}/bin/tmux list-sessions -F '#{session_attached} #{session_id}' \
-            | awk '/^0/{print $2}' \
-            | xargs -n 1 tmux kill-session -t
-        '';
-      };
-      initExtra =
-        # sh
-        ''
-          zstyle ':completion:*' menu no
-          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-          zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-          zstyle ':completion:*:descriptions' format '[%d]'
-          zstyle ':completion:*:git-checkout:*' sort false
-          zstyle ':fzf-tab:*' use-fzf-default-opts yes
-          zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
-          zstyle ':fzf-tab:*' popup-pad 30 0
-          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color always $realpath'
-
-          function lf() {
-            local tmp="$(mktemp -t 'yazi-cwd.XXXXXX')" cwd
-            yazi "$@" --cwd-file="$tmp"
-            if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-            builtin cd -- "$cwd"
-            fi
-            rm -f -- "$tmp"
-          }
-
-          function ,() {
-            nix run "nixpkgs#$1" -- $(shift; echo "$@")
-          }
-
-          eval "$(starship init zsh)"
-        '';
-    };
-  };
-}
-*/
-
