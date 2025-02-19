dd3954e3698d502b2b5d46dadaf8d9b3bb42a0bf
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-02-19 21:55:08 +0100

    ß

--- hosts/mercury/keys.nix
+++ hosts/mercury/keys.nix
@@ -3,5 +3,6 @@
 in {
   users.users.${username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgGq/UF0qlgr76WWLTocw+U607Dlu7SYFcrGqstWGGy kkts@desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWj17DwiR3LomTeZU2WQ07CXfymAnRYqzu99ZvMyaHN u0_a467@localhost"
   ];
 }
