65d78ce514bb313c32d7559679f86e7e4e2ecdf2
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-12-26 21:30:09 +0100

    feat: configure nix

---
+++ modules/core/services/nix.nix
@@ -0,0 +1,64 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (inputs) nixpkgs;
+  inherit (lib.lists) singleton;
+  inherit (pkgs.writers) writeJSON;
+in {
+  nix = {
+    package = pkgs.lix;
+    nixPath = ["nixpkgs=${nixpkgs}"];
+    settings = {
+      experimental-features = [
+        "auto-allocate-uids"
+        "cgroups"
+        "coerce-integers"
+        "flakes"
+        "nix-command"
+        "pipe-operator"
+      ];
+
+      auto-allocate-uids = true;
+      use-cgroups = true;
+      allowed-users = ["@wheel"];
+      trusted-users = ["@wheel"];
+      accept-flake-config = false;
+
+      keep-outputs = true;
+      warn-dirty = false;
+
+      flake-registry = writeJSON "flake-registry-empty.json" {
+        flakes = [];
+        version = 2;
+      };
+    };
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 14d";
+      dates = ["Sat *-*-* 03:00"];
+      randomizedDelaySec = "15min";
+    };
+    optimise = {
+      automatic = true;
+      dates = ["Sat *-*-* 04:00"];
+      randomizedDelaySec = "15min";
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowAliases = false;
+      allowUnfree = true;
+    };
+    overlays = singleton (_: prev: {
+      inherit
+        (prev.lixPackageSets.latest)
+        lix
+        nix-direnv
+        ;
+    });
+  };
+}
