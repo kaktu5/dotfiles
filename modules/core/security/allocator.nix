cac87193e8610817af9d5146b25b35e5b4ff71bd
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-01-06 17:42:05 +0100

    feat: grapheneos allocator

---
+++ modules/core/security/allocator.nix
@@ -0,0 +1,6 @@
+{
+  environment.memoryAllocator.provider = "graphene-hardened";
+
+  # raised to handle the large number of guard pages that the hardened allocator creates
+  boot.kernel.sysctl."vm.max_map_count" = 1024 * 1024;
+}
