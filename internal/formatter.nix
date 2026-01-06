a60f31c112434f079cdf9b5653640a65280ae5d2
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-01-06 17:42:05 +0100

    chore: switch from mdformat to deno fmt

--- internal/formatter.nix
+++ internal/formatter.nix
@@ -8,11 +8,11 @@ in
   writeShellApplication {
     name = "dotfiles-nix3-fmt-wrapper";
     runtimeInputs = attrValues {
-      inherit (pkgs) alejandra fd mdformat;
+      inherit (pkgs) alejandra deno fd;
       inherit (pkgs.kdePackages) qtdeclarative;
     };
     text = ''
-      fd "$@" -t f -e md -X mdformat --wrap 120 '{}'
+      fd "$@" -t f -e md -X deno fmt '{}'
       fd "$@" -t f -e nix -E npins/ -X alejandra --quiet '{}'
       fd "$@" -t f -e qml -x qmlformat --inplace '{}'
     '';
