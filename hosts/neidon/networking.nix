d5c1f1a2d750e6da100e88f5e63cbbc6395b9b30
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    setup networking in initrd

--- hosts/neidon/networking.nix
+++ hosts/neidon/networking.nix
@@ -1,9 +1,21 @@
-{
-  systemd.network.networks."0-lan" = {
-    matchConfig.Name = "enp1s0f0";
+{config, ...}: let
+  inherit (config.networking) hostName;
+
+  device = "enp1s0f0";
+  address = "10.0.0.2";
+  gateway = "10.0.0.1";
+in {
+  boot = {
+    kernelParams = ["ip=${address}::${gateway}:255.255.255.0:${hostName}:${device}:none"];
+
+    initrd.kernelModules = ["r8169"];
+  };
+
+  systemd.network.networks."0-${device}" = {
+    matchConfig.Name = device;
     networkConfig = {
-      Address = "10.0.0.2/24";
-      Gateway = "10.0.0.1";
+      Address = "${address}/24";
+      Gateway = gateway;
     };
   };
 }
