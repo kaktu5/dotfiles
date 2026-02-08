7b2f7a832868a42f0cd6c48f87d3beeeaa2dbe71
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    switch to tack

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
+  outputs = {self}: let
     systems = ["aarch64-linux" "x86_64-linux"];
+
+    inputs = import ./.tack;
 
     lib = import ./lib {inherit inputs self;};
     inherit (lib.attrsets) mapAttrs zipAttrsWith;
@@ -16,9 +14,9 @@
       |> zipAttrsWith (_: foldl' (a: b: a // b) {});
   in
     mapSystems systems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
     in {
-      devShells.default = import ./internal/devshell.nix {inherit lib pkgs self system;};
+      devShells.default = import ./internal/devshell.nix {inherit inputs lib pkgs self system;};
 
       formatter = import ./internal/formatter.nix {inherit lib pkgs;};
     })
@@ -29,65 +27,4 @@
 
       vaultix = import ./internal/vaultix.nix {inherit inputs self systems;};
     };
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    nixexprs.url = "github:kaktu5/nixexprs";
-
-    hjem = {
-      url = "github:feel-co/hjem";
-      inputs = {
-        nix-darwin.follows = "";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    microvm = {
-      url = "github:microvm-nix/microvm.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        spectrum.follows = "";
-      };
-    };
-    preservation.url = "github:nix-community/preservation";
-    stash = {
-      url = "github:notashelf/stash";
-      inputs = {
-        crane.follows = "crane";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    tuigreet = {
-      url = "github:notashelf/tuigreet";
-      inputs = {
-        crane.follows = "crane";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    vaultix = {
-      url = "github:milieuim/vaultix";
-      inputs = {
-        crane.follows = "crane";
-        flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-        nixpkgs.follows = "nixpkgs";
-        pre-commit-hooks.inputs = {
-          gitignore.inputs.nixpkgs.follows = "nixpkgs";
-          nixpkgs.follows = "nixpkgs";
-        };
-        rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-      };
-    };
-
-    dnscrypt-settings = {
-      url = "github:quad9dns/dnscrypt-settings";
-      flake = false;
-    };
-    oisd = {
-      url = "github:sjhgvr/oisd";
-      flake = false;
-    };
-
-    # not used directly, pinned only to deduplicate transitive dependencies
-    crane.url = "github:ipetkov/crane";
-  };
 }
