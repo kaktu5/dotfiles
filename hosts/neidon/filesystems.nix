4f9415b2a93ad8a4fe018d2baafe981c87059647
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    add zram-swap module

--- hosts/neidon/filesystems.nix
+++ hosts/neidon/filesystems.nix
@@ -1,6 +1,4 @@
-{lib, ...}: let
-  inherit (lib.lists) singleton;
-in {
+{
   fileSystems = {
     "/boot" = {
       device = "/dev/disk/by-uuid/12CE-A600";
@@ -18,14 +16,9 @@ in {
     };
   };
 
-  swapDevices = singleton {
-    device = "/dev/disk/by-partuuid/d9eb2e78-4ac3-4d08-9b65-07c05a119d04";
-    randomEncryption = {
-      enable = true;
-      allowDiscards = true;
-      keySize = 512;
-      sectorSize = 4096;
-    };
-    discardPolicy = "once";
+  kkts.zramSwap = {
+    enable = true;
+    size = 4 * 1024 * 1024 * 1024;
+    writeback.device = "/dev/disk/by-partuuid/d9eb2e78-4ac3-4d08-9b65-07c05a119d04";
   };
 }
