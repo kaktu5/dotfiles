2e5eba7834fb6cd83dbc0d24f057c0f9a14029e8
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    feat: services module

---
+++ modules/core/services/module.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ./systemd.nix
+
+    ./dbus.nix
+    ./disable-speechd.nix
+    ./dnscrypt-proxy.nix
+    ./greetd.nix
+    ./nix.nix
+    ./openntpd.nix
+    ./openssh.nix
+    ./userborn.nix
+  ];
+}
