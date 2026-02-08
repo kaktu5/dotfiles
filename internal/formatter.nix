4abd662a9d63e6ed5fb676920322f4cbc492c014
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    switch back to mdformat

--- internal/formatter.nix
+++ internal/formatter.nix
@@ -8,11 +8,11 @@ in
   writeShellApplication {
     name = "dotfiles-nix3-fmt-wrapper";
     runtimeInputs = attrValues {
-      inherit (pkgs) alejandra deno fd;
+      inherit (pkgs) alejandra fd mdformat;
       inherit (pkgs.kdePackages) qtdeclarative;
     };
     text = ''
-      fd "$@" -t f -e md -X deno fmt '{}'
+      fd "$@" -t f -e md -X mdformat '{}'
       fd "$@" -t f -e nix -X alejandra --quiet '{}'
       fd "$@" -t f -e qml -x qmlformat --inplace '{}'
     '';
