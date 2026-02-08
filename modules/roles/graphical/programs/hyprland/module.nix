c2a00dca0c656dde499bdd67f3fcf26c0a53fb7a
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    configure hyprland using HYPRLAND_CONFIG env var

--- modules/roles/graphical/programs/hyprland/module.nix
+++ modules/roles/graphical/programs/hyprland/module.nix
@@ -1,11 +1,18 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (config.kkts.meta) userName;
   inherit (config.kkts.programs.hyprland) settings;
   inherit (lib.kkts.formats) hyprlang;
+  inherit (pkgs.writers) writeText;
+
+  HYPRLAND_CONFIG =
+    settings
+    |> hyprlang.generate {priorityKeys = ["bezier" "name" "output"];}
+    |> writeText "hyprland-conf";
 in {
   imports = [
     ./binds.nix
@@ -21,17 +28,15 @@ in {
     withUWSM = true;
   };
 
-  hjem.users.${userName} = {
-    environment.sessionVariables = {
-      CLUTTER_BACKEND = "wayland";
-      GDK_BACKEND = "wayland,x11,*";
-      QT_QPA_PLATFORM = "wayland;xcb";
-      SDL_VIDEODRIVER = "wayland";
-    };
+  hjem.users.${userName}.environment.sessionVariables = {
+    inherit HYPRLAND_CONFIG;
 
-    xdg.config.files."hypr/hyprland.conf" = {
-      generator = hyprlang.generate {priorityKeys = ["bezier" "name" "output"];};
-      value = settings;
-    };
+    XCURSOR_SIZE = 32;
+    XCURSOR_THEME = "BreezeX";
+
+    CLUTTER_BACKEND = "wayland";
+    GDK_BACKEND = "wayland,x11,*";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    SDL_VIDEODRIVER = "wayland";
   };
 }
