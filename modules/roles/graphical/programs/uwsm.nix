0840269e1e7fd2c22e56c4e8f1d8fc509d1c9de2
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    configure UWSM

---
+++ modules/roles/graphical/programs/uwsm.nix
@@ -0,0 +1,12 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (config.hjem.users.${userName}.environment) sessionVariables;
+  inherit (config.kkts.meta) userName;
+  inherit (lib.strings) concatMapAttrsStringSep;
+in {
+  hjem.users.${userName}.xdg.config.files."uwsm/env".text =
+    sessionVariables |> concatMapAttrsStringSep "\n" (n: v: "export ${n}=\"${v}\"");
+}
