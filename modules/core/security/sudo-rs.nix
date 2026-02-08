00086fe517ae7983cf9369f5d761dcc216b85a74
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    refactor:

--- modules/core/security/sudo-rs.nix
+++ modules/core/security/sudo-rs.nix
@@ -3,12 +3,14 @@
     sudo-rs = {
       enable = true;
       execWheelOnly = true;
+
       extraConfig = ''
         Defaults !lecture
         Defaults pwfeedback
       '';
     };
-    sudo.enable = false;
+
     shadow.enable = false;
+    sudo.enable = false;
   };
 }
