add0eb1bbb0d94624a189bf482c01ea8aca4ec22
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    disable plasma polkit agent notification

--- modules/traits/graphical/services/plasma-polkit-agent.nix
+++ modules/traits/graphical/services/plasma-polkit-agent.nix
@@ -6,18 +6,26 @@
 }: let
   inherit (config.hjem.users.${userName}.environment) sessionVariables;
   inherit (config.kkts.meta) userName;
+  inherit (lib.generators) toINI;
   inherit (lib.kkts.systemd) mkGraphicalTargetService;
   inherit (pkgs.kdePackages) polkit-kde-agent-1;
 in {
-  hjem.users.${userName}.systemd.services.plasma-polkit-agent = mkGraphicalTargetService {
-    environment = {
-      inherit (sessionVariables) QT_PLUGIN_PATH QT_QPA_PLATFORMTHEME;
+  hjem.users.${userName} = {
+    systemd.services.plasma-polkit-agent = mkGraphicalTargetService {
+      environment = {
+        inherit (sessionVariables) QT_PLUGIN_PATH QT_QPA_PLATFORMTHEME;
+      };
+
+      serviceConfig = {
+        ExecStart = polkit-kde-agent-1 + /libexec/polkit-kde-authentication-agent-1;
+        BusName = "org.kde.polkit-kde-authentication-agent-1";
+        Slice = "background.slice";
+      };
     };
 
-    serviceConfig = {
-      ExecStart = polkit-kde-agent-1 + /libexec/polkit-kde-authentication-agent-1;
-      BusName = "org.kde.polkit-kde-authentication-agent-1";
-      Slice = "background.slice";
+    xdg.data.files."knotifications6/polkit-kde-authentication-agent-1.notifyrc" = {
+      generator = toINI {};
+      value."Event/authenticate".Action = "None";
     };
   };
 }
