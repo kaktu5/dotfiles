570227b597d5111c711dcdc0a3c98e1cdcc1c359
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    set env vars to prefer wayland

--- modules/roles/graphical/programs/hyprland/module.nix
+++ modules/roles/graphical/programs/hyprland/module.nix
@@ -21,8 +21,17 @@ in {
     withUWSM = true;
   };
 
-  hjem.users.${userName}.xdg.config.files."hypr/hyprland.conf" = {
-    generator = hyprlang.generate {priorityKeys = ["bezier" "name" "output"];};
-    value = settings;
+  hjem.users.${userName} = {
+    environment.sessionVariables = {
+      CLUTTER_BACKEND = "wayland";
+      GDK_BACKEND = "wayland,x11,*";
+      QT_QPA_PLATFORM = "wayland;xcb";
+      SDL_VIDEODRIVER = "wayland";
+    };
+
+    xdg.config.files."hypr/hyprland.conf" = {
+      generator = hyprlang.generate {priorityKeys = ["bezier" "name" "output"];};
+      value = settings;
+    };
   };
 }
