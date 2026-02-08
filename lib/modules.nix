5b54a177c7e3e7ee415cea0df598ca7e494bb773
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    use lib self reference

--- lib/modules.nix
+++ lib/modules.nix
@@ -1,27 +1,27 @@
 {lib}: let
   inherit (builtins) readDir;
   inherit (lib.attrsets) mapAttrsToList;
-  inherit (lib.fixedPoints) fix;
   inherit (lib.lists) concatLists;
   inherit (lib.strings) hasSuffix;
   inherit (lib.trivial) pathExists;
-in
-  fix (self: {
-    modulesFromDirRec = dir: let
-      defaultPath = dir + /module.nix;
-      processDir = dir:
-        readDir dir
-        |> mapAttrsToList (name: type: let
-          path = dir + /${name};
-        in
-          if type == "directory"
-          then self.modulesFromDirRec path
-          else if type == "regular" && hasSuffix ".nix" name
-          then [path]
-          else [])
-        |> concatLists;
-    in
-      if pathExists defaultPath
-      then [defaultPath]
-      else processDir dir;
-  })
+
+  self = lib.kkts.modules;
+in {
+  modulesFromDirRec = dir: let
+    defaultPath = dir + /module.nix;
+    processDir = dir:
+      readDir dir
+      |> mapAttrsToList (name: type: let
+        path = dir + /${name};
+      in
+        if type == "directory"
+        then self.modulesFromDirRec path
+        else if type == "regular" && hasSuffix ".nix" name
+        then [path]
+        else [])
+      |> concatLists;
+  in
+    if pathExists defaultPath
+    then [defaultPath]
+    else processDir dir;
+}
