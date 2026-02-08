2139afac3f515e801bff6a84230abec9e608b6e2
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    override systemd package

--- modules/core/services/systemd.nix
+++ modules/core/services/systemd.nix
@@ -1,8 +1,32 @@
 {
+  config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (lib.generators) toKeyValue;
+
+  package = pkgs.systemd.override (let
+    withCoredump = config.systemd.coredump.enable;
+    withResolved = config.services.resolved.enable || config.boot.initrd.services.resolved.enable;
+    withTimesyncd = config.services.timesyncd.enable;
+  in {
+    inherit withCoredump;
+    withHomed = false;
+    withHostnamed = false;
+    withImportd = false;
+    withLocaled = false;
+    withPasswordQuality = false;
+    withPortabled = false;
+    withRemote = false;
+    inherit withResolved;
+    withSysupdate = false;
+    withTimedated = false;
+    inherit withTimesyncd;
+    withUserDb = false;
+    withNss = withResolved;
+    withKexectools = false;
+  });
 
   timeoutConfig = {
     DefaultTimeoutStartSec = "10s";
@@ -14,6 +38,8 @@ in {
   boot.initrd.systemd.settings.Manager = timeoutConfig;
 
   systemd = {
+    inherit package;
+
     enableEmergencyMode = false;
 
     settings.Manager = timeoutConfig;
