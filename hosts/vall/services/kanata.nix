62d2fe501e3faf5faa81d44769092df326e8a053
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    rename keyboard

--- hosts/vall/services/kanata.nix
+++ hosts/vall/services/kanata.nix
@@ -4,7 +4,7 @@
   services.kanata = {
     enable = true;
 
-    keyboards.builtin = {
+    keyboards.internal = {
       devices = ["/dev/input/by-path/platform-i8042-serio-0-event-kbd"];
 
       extraArgs = ["--nodelay"];
