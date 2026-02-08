930a1830dfb1c482653218269cda81151f91330e
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    disable most system.tools

---
+++ modules/core/system/tools.nix
@@ -0,0 +1,10 @@
+{
+  system.tools = {
+    nixos-build-vms.enable = false;
+    nixos-enter.enable = false;
+    nixos-generate-config.enable = false;
+    nixos-install.enable = false;
+    nixos-option.enable = false;
+    nixos-version.enable = false;
+  };
+}
