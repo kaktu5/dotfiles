1ff0b12f0398f3ec4bd32bcdcaa2ee6f2f4e33ba
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-02-08 16:45:43 +0100

    hyprlang generator

--- lib/formats.nix
+++ lib/formats.nix
@@ -1,6 +1,6 @@
 {lib}: let
-  inherit (lib.attrsets) isDerivation mapAttrsToList;
-  inherit (lib.lists) foldl';
+  inherit (lib.attrsets) attrNames isAttrs isDerivation mapAttrsToList;
+  inherit (lib.lists) elem filter foldl' subtractLists toList;
   inherit (lib.strings) concatMapStringsSep concatStringsSep hasPrefix removePrefix splitString trim typeOf;
   inherit (lib.trivial) boolToString;
 
@@ -35,6 +35,39 @@ in {
     };
   in
     (str |> splitString "\n" |> foldl' parseLine initialAcc).stamps;
+
+  hyprlang.generate = {priorityKeys ? []}: attrs: let
+    orderKeys = keys: let
+      priorityKeys' = priorityKeys |> filter (key: elem key keys);
+      restKeys = subtractLists priorityKeys' keys;
+    in
+      priorityKeys' ++ restKeys;
+
+    mkValue = v: let
+      cases = {
+        bool = boolToString v;
+        float = toString v;
+        int = "${v}";
+        list = concatMapStringsSep "," mkValue v;
+        null = "";
+      };
+    in
+      cases.${typeOf' v} or (toString v);
+
+    mkCategory = attrs:
+      attrs
+      |> attrNames
+      |> orderKeys
+      |> map (key:
+        attrs.${key}
+        |> toList
+        |> concatMapStringsSep "\n" (v:
+          if isAttrs v && !isDerivation v
+          then "${key}{\n${mkCategory v}\n}"
+          else "${key}=${mkValue v}"))
+      |> concatStringsSep "\n";
+  in
+    mkCategory attrs;
 
   nuon.generate = {}: attrs: let
     mkValue = v: let
