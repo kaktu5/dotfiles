be6fb0e48fac93713a2283d8d997e1deed86d879
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    pin latest working xanmod+zfs

--- modules/traits/workstation/boot/kernel.nix
+++ modules/traits/workstation/boot/kernel.nix
@@ -1,3 +1,10 @@
-{pkgs, ...}: {
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+{
+  config,
+  inputs,
+  ...
+}: let
+  inherit (config.nixpkgs.hostPlatform) system;
+  inherit (inputs.nixpkgs-xanmod.legacyPackages.${system}) linuxPackages_xanmod_latest;
+in {
+  boot.kernelPackages = linuxPackages_xanmod_latest;
 }
