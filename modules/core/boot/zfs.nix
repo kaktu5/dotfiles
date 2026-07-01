626b62d6fb0812d51d35676f90e79474a27c1359
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    increase size of zfs_arc_sys_free

--- modules/core/boot/zfs.nix
+++ modules/core/boot/zfs.nix
@@ -1,3 +1,7 @@
 {
-  boot.zfs.devNodes = "/dev/disk/by-partuuid";
+  boot = {
+    zfs.devNodes = "/dev/disk/by-partuuid";
+
+    extraModprobeConfig = "options zfs zfs_arc_sys_free=${2 * 1024 * 1024 * 1024}";
+  };
 }
