deb355eac91e3e225e4d8495e45b736668343117
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    setup libvirtd

---
+++ modules/traits/workstation/programs/libvirtd.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}: let
+  inherit (pkgs) virtiofsd;
+in {
+  preservation.preserveAt."/persist".directories = ["/var/lib/libvirt"];
+
+  networking.firewall.trustedInterfaces = ["virbr0"];
+
+  virtualisation.libvirtd = {
+    enable = true;
+
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+    parallelShutdown = 4;
+
+    qemu = {
+      runAsRoot = false;
+
+      swtpm.enable = true;
+
+      vhostUserPackages = [virtiofsd];
+    };
+  };
+
+  programs.virt-manager.enable = true;
+}
