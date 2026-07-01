bf38857e1fe1e114a166cc7902212a94e124b2b6
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    persist neovim cache

--- modules/traits/workstation/programs/neovim.nix
+++ modules/traits/workstation/programs/neovim.nix
@@ -4,11 +4,14 @@
   lib,
   ...
 }: let
+  inherit (config.hjem.users.${userName}) xdg;
   inherit (config.kkts.meta) userName;
   inherit (config.nixpkgs.hostPlatform) system;
   inherit (inputs.nf.packages.${system}) neovim;
   inherit (lib.meta) getExe;
 in {
+  preservation.preserveAt."/persist".users.${userName}.directories = ["${xdg.cache.directory}/neovim"];
+
   users.users.${userName}.packages = [neovim];
 
   hjem.users.${userName}.environment.sessionVariables = {
