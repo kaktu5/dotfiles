eb76126e2645e4e736e4c9fabfcd5ce74f48e8ab
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    persist firefox cache

--- modules/traits/workstation/programs/firefox.nix
+++ modules/traits/workstation/programs/firefox.nix
@@ -3,10 +3,14 @@
   pkgs,
   ...
 }: let
+  inherit (config.hjem.users.${userName}) xdg;
   inherit (config.kkts.meta) userName;
   inherit (pkgs) firefox;
 in {
-  preservation.preserveAt."/persist".users.${userName}.directories = [".config/mozilla/firefox"];
+  preservation.preserveAt."/persist".users.${userName}.directories = [
+    "${xdg.cache.directory}/mozilla/firefox"
+    "${xdg.config.directory}/mozilla/firefox"
+  ];
 
   users.users.${userName}.packages = [firefox];
 }
