6391122eb1cfeb7356891e7899adecf2ba49f8da
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    basic nushell configuration

--- resources/bootstrap/default.nix
+++ resources/bootstrap/default.nix
@@ -8,18 +8,29 @@ let
 
   inherit (pkgs) gitMinimal mkShellNoCC nushell;
   inherit (pkgs.lixPackageSets.latest) lix;
+
+  NIX_CONFIG = ''
+    experimental-features = coerce-integers flakes nix-command pipe-operator
+    log-format = multiline-with-logs
+    log-lines = 64
+    use-xdg-base-directories = true
+  '';
+
+  nushellConfig = ''
+    {
+      edit_mode: "vi",
+      filesize: {unit: "binary"},
+      show_banner: false,
+      table: {mode: "none"},
+    }
+  '';
 in
   mkShellNoCC {
     name = "dotfiles-bootstrap-devshell";
 
-    env.NIX_CONFIG = ''
-      experimental-features = coerce-integers flakes nix-command pipe-operator
-      log-format = multiline-with-logs
-      log-lines = 64
-      use-xdg-base-directories = true
-    '';
+    env = {inherit NIX_CONFIG;};
 
     packages = [gitMinimal lix nushell];
 
-    shellHook = "exec nu";
+    shellHook = "exec nu --execute '$env.config = ($env.config | merge ${nushellConfig})'";
   }
