08ced8b4dc36028b6a4019cdd2c4af5ab20f791c
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    setup lanzaboote

---
+++ modules/traits/bare-metal/boot/lanzaboote.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (config.kkts.meta) userName;
+  inherit (inputs) lanzaboote;
+  inherit (lib.lists) singleton;
+  inherit (pkgs) sbctl;
+
+  cfg = config.boot.lanzaboote;
+in {
+  imports = [lanzaboote.nixosModules.default];
+
+  preservation.preserveAt."/persist".directories = singleton {
+    directory = cfg.pkiBundle;
+    mode = "u=rwx,g=,o=";
+  };
+
+  users.users.${userName}.packages = [sbctl];
+
+  boot = {
+    loader.efi.canTouchEfiVariables = true;
+
+    lanzaboote = {
+      enable = true;
+
+      configurationLimit = 8;
+
+      pkiBundle = "/var/lib/sbctl";
+
+      settings = {
+        timeout = 0;
+        console-mode = "max";
+        editor = false;
+      };
+
+      bootCounting.initialTries = 1;
+
+      autoGenerateKeys.enable = true;
+      autoEnrollKeys = {
+        enable = true;
+        autoReboot = true;
+      };
+    };
+  };
+}
