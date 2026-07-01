393ff46814559a384585e4db49851dfb447460e9
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    who needs ROCm anyway

--- modules/options/hardware/gpu/amd.nix
+++ modules/options/hardware/gpu/amd.nix
@@ -1,13 +1,10 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: let
   inherit (lib.modules) mkIf mkMerge;
   inherit (lib.options) mkEnableOption;
-  inherit (pkgs) symlinkJoin;
-  inherit (pkgs.rocmPackages) clr hipblas rocblas;
 
   cfg = config.kkts.hardware.gpu.amd;
 in {
@@ -15,8 +12,6 @@ in {
     enable = mkEnableOption "amd gpu support";
 
     enable32Bit = mkEnableOption "enable 32-bit support";
-
-    enableRocmSupport = mkEnableOption "enable ROCm support" // {default = true;};
   };
 
   config = mkIf cfg.enable (mkMerge [
@@ -31,19 +26,6 @@ in {
 
     (mkIf cfg.enable32Bit {
       hardware.graphics = {inherit (cfg) enable32Bit;};
-    })
-
-    (mkIf cfg.enableRocmSupport {
-      nixpkgs.config.rocmSupport = true;
-
-      hardware.amdgpu.opencl.enable = true;
-
-      systemd.tmpfiles.rules = let
-        rocmEnv = symlinkJoin {
-          name = "rocm-env";
-          paths = [clr hipblas rocblas];
-        };
-      in ["L+ /opt/rocm - - - - ${rocmEnv}"];
     })
   ]);
 }
