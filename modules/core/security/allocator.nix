57179c9343387acbe78b01e1732d1e4cc65623e3
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    disable grapheneos allocator for now

--- modules/core/security/allocator.nix
+++ modules/core/security/allocator.nix
@@ -1,6 +1,7 @@
+# TODO: find a way to exclude apps from a hardened allocator
 {
-  environment.memoryAllocator.provider = "graphene-hardened";
+  # environment.memoryAllocator.provider = "graphene-hardened";
 
   # raised to handle the large number of guard pages that the hardened allocator creates
-  boot.kernel.sysctl."vm.max_map_count" = 1024 * 1024;
+  # boot.kernel.sysctl."vm.max_map_count" = 1024 * 1024;
 }
