a407d28524d9ae0e0454fcc21d6a20c7713adb1b
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    base gaming profile

---
+++ modules/profiles/gaming/config.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.kkts.dag) entryAnywhere;
+  inherit (lib.meta) getExe';
+  inherit (lib.modules) mkIf;
+  inherit (pkgs) wireplumber;
+
+  cfg = config.kkts.profiles.gaming;
+in
+  mkIf cfg.enable {
+    boot.kernelModules = ["ntsync"];
+
+    kkts.programs.hyprland.extraEntries.mute-unfocused-game-windows = entryAnywhere ''
+      do
+        local muted_pids = {}
+
+        local function update_mutes(awin)
+          for _, win in ipairs(hl.get_windows()) do
+            if win.xdg_tag == "proton-game" then
+              local should_be_muted = (win.pid ~= awin.pid)
+              if muted_pids[win.pid] ~= should_be_muted then
+                hl.exec_cmd(string.format("${getExe' wireplumber "wpctl"} set-mute -p %d %s", win.pid, should_be_muted and "1" or "0"))
+                muted_pids[win.pid] = should_be_muted
+              end
+            end
+          end
+        end
+
+        hl.on("window.active", update_mutes)
+        hl.on("window.open", function() update_mutes(hl.get_active_window()) end)
+        hl.on("window.close", function(win) muted_pids[win.pid] = nil end)
+      end
+    '';
+  }
--- modules/profiles/gaming/module.nix
+++ modules/profiles/gaming/module.nix
@@ -1,11 +1,19 @@
-{lib, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   inherit (lib.options) mkEnableOption;
+
+  cfg = config.kkts.profiles.gaming;
 in {
   imports = [
+    ./config.nix
     ./steam.nix
   ];
 
   options.kkts.profiles.gaming = {
+    enable = mkEnableOption "gaming profile";
     steam.enable = mkEnableOption "steam";
   };
 }
