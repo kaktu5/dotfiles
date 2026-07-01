d8aa34569f2538aead25eade0990f2c27e46f36d
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-07-01 19:25:43 +0200

    fix permissions and persistance

--- modules/traits/workstation/programs/openssh/module.nix
+++ modules/traits/workstation/programs/openssh/module.nix
@@ -1,10 +1,12 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
   inherit (config.kkts.meta) userName;
   inherit (config.vaultix) secrets;
+  inherit (lib.lists) singleton;
   inherit (pkgs) openssh;
 in {
   vaultix.secrets.id = {
@@ -13,15 +15,28 @@ in {
     group = "users";
   };
 
+  preservation.preserveAt."/persist".users.${userName}.files = singleton {
+    file = ".ssh/known_hosts";
+    mode = "600";
+  };
+
   users.users.${userName}.packages = [openssh];
 
   hjem.users.${userName}.files = {
-    ".ssh/ssh-key.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrm08yLyJn1TpTvnEuyyuSp60hD2Z8oOXZgsA/sbHPa";
+    ".ssh/ssh-key.pub" = {
+      text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrm08yLyJn1TpTvnEuyyuSp60hD2Z8oOXZgsA/sbHPa";
+      type = "copy";
+      permissions = "644";
+    };
 
-    ".ssh/config".text = ''
-      Host *
-        IdentityFile ${secrets.id.path}
-        IdentitiesOnly yes
-    '';
+    ".ssh/config" = {
+      text = ''
+        Host *
+          IdentityFile ${secrets.id.path}
+          IdentitiesOnly yes
+      '';
+      type = "copy";
+      permissions = "600";
+    };
   };
 }
